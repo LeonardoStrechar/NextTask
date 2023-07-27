c9bfc6e5351215256fcc3b83634ab0d0968221d0
LeonardoStrechar <strechar21@gmail.com>
2023-07-27 11:04:15 -0300

    Feat: add logic for login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,13 @@ import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/header/header'
 
+import { SessionProvider } from 'next-auth/react'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Header />
-    <Component {...pageProps} />
-  </>
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
